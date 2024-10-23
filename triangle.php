125eee9575d286e9d2b88645c5cba7c301614207
JandroSantos <150789377+JandroSantos@users.noreply.github.com>
2024-10-23 10:31:09 +0200

    En triangle.php hacemos la llamada al metodo primero instanciamos el objeto triangulo y deajo llamamos al método pasandole la altura

---
+++ triangle.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+ 
+include "clases/triangleGenerator.php";
+ 
+$triangulo = new TriangleGenerator();
+$triangulo->generate_triangle(6);
+ 
+?>
+ 
+</body>
+</html>
